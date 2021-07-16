71f72dd7de92dbe916082de85323de70f65134fd
Dung Ta Van <itprono3@gmail.com>
2021-07-16 22:42:47 +0700

    update bridging-header

--- ezyfox-client-bridging-header.h
+++ ezyfox-client-bridging-header.h
@@ -1,17 +1,17 @@
 //
-//  hello-swift-Bridging-Header.h.h
+//  ezyfox-client-bridging-header.h
 //  hello-swift
 //
 //  Created by Dung Ta Van on 10/29/18.
 //  Copyright © 2018 Young Monkeys. All rights reserved.
 //
 
-#ifndef hello_swift_Bridging_Header_h_h
-#define hello_swift_Bridging_Header_h_h
+#ifndef ezyfox_client_bridging_header_h
+#define ezyfox_client_bridging_header_h
 
 #import "EzyClientProxy.h"
 #import "EzyEncryptionProxy.h"
 #import "math/EzyNSNumber.h"
 #import "util/NSByteArray.h"
 
-#endif /* hello_swift_Bridging_Header_h_h */
+#endif /* ezyfox_client_bridging_header_h */
